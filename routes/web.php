80cfcbc105c145ad6a70c310b92b9d11121bd7c1
João Roberto <joaocte@gmail.com>
2021-12-24 18:37:28 -0300

    aplicacao laravel

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{PrincipalController, SobreNosController, ContatoController};
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', [PrincipalController::class, 'Principal']);
+Route::get('/contato', [ContatoController::class, 'Contato']);
+Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos']);
+
+Route::get('contato/{nome}', function (string $nome) {
+
+    echo 'Estamos aqui ' .$nome;
+});
