c5696b1cc140ac8fb576b48f2b80de1835701120
João Roberto <joaocte@gmail.com>
2021-12-28 21:32:28 -0300

    loops

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,23 +1,23 @@
 <h3>Fornecedor</h3>
 
 @php
-    /*
-    if(!condicao) {} //enquanto executa se o retorno for true
-    */
-    /*
-    if(isset($variavel)) {} //retornar true se a variável estiver definida
-    */
-    /*
-    if(empty($variavel)) {} //retornar true se a variável estiver vazia
-    - ''
-    - 0
-    - 0.0
-    - '0'
-    - null
-    - false
-    - array()
-    - $var
-    */
+/*
+if(!condicao) {} //enquanto executa se o retorno for true
+*/
+/*
+if(isset($variavel)) {} //retornar true se a variável estiver definida
+*/
+/*
+if(empty($variavel)) {} //retornar true se a variável estiver vazia
+- ''
+- 0
+- 0.0
+- '0'
+- null
+- false
+- array()
+- $var
+*/
 
 @endphp
 
@@ -29,11 +29,11 @@ Fornecedor: {{ $fornecedores[0]['nome'] }}
 Status: {{ $fornecedores[0]['status'] }}
 <br>
 @if( !($fornecedores[0]['status'] == 'S') )
-    Fornecedor inativo
+Fornecedor inativo
 @endif
 <br>
 @unless($fornecedores[0]['status'] == 'S') <!-- se o retorno da condição for false -->
-    Fornecedor inativo
+Fornecedor inativo
 @endunless
 <br>
 
@@ -41,115 +41,133 @@ Status: {{ $fornecedores[0]['status'] }}
 <h3>isset</h3>
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-    @endisset
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@isset($fornecedores[0]['cnpj'])
+CNPJ: {{ $fornecedores[0]['cnpj'] }}
+@endisset
 @endisset
 <hr>
 <h3>empty</h3>
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[0]['status'] }}
-    <br>
-    @isset($fornecedores[0]['cnpj'])
-        CNPJ: {{ $fornecedores[0]['cnpj'] }}
-        @empty($fornecedores[0]['cnpj'])
-            - Vazio
-        @endempty
-    @endisset
+Fornecedor: {{ $fornecedores[0]['nome'] }}
+<br>
+Status: {{ $fornecedores[0]['status'] }}
+<br>
+@isset($fornecedores[0]['cnpj'])
+CNPJ: {{ $fornecedores[0]['cnpj'] }}
+@empty($fornecedores[0]['cnpj'])
+- Vazio
+@endempty
+@endisset
 @endisset
 
 <hr>
 <h3>switch</h3>
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[1]['nome'] }}
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Cnpj não informado' }}
-    <!--
-        $variável testada não estiver definida (isset)
-        ou
-        $variável testada possui o valor null
-    -->
-    <br>
-    Telefone: ({{ $fornecedores[0]['ddd'] ?? '' }}) {{ $fornecedores[0]['telefone'] ?? '' }}
-    @switch($fornecedores[0]['ddd'])
-        @case ('11')
-            São Paulo - SP
-            @break
-        @case ('32')
-            Juiz de Fora - MG
-            @break
-        @case ('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não indentificado
-    @endswitch
+Fornecedor: {{ $fornecedores[1]['nome'] }}
+<br>
+Status: {{ $fornecedores[1]['status'] }}
+<br>
+CNPJ: {{ $fornecedores[1]['cnpj'] ?? 'Cnpj não informado' }}
+<!--
+$variável testada não estiver definida (isset)
+ou
+$variável testada possui o valor null
+-->
+<br>
+Telefone: ({{ $fornecedores[0]['ddd'] ?? '' }}) {{ $fornecedores[0]['telefone'] ?? '' }}
+@switch($fornecedores[0]['ddd'])
+@case ('11')
+São Paulo - SP
+@break
+@case ('32')
+Juiz de Fora - MG
+@break
+@case ('85')
+Fortaleza - CE
+@break
+@default
+Estado não indentificado
+@endswitch
 @endisset
 
 @isset($fornecedores)
-    <hr>
-    <h3>for</h3>
-    @for($i = 0; isset($fornecedores[$i]); $i++)
-        Fornecedor: {{ $fornecedores[$i]['nome'] }}
-        <br>
-        Status: {{ $fornecedores[$i]['status'] }}
-        <br>
-        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
-        <br>
-        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
-         @switch($fornecedores[$i]['ddd'])
-        @case ('11')
-            São Paulo - SP
-            @break
-        @case ('32')
-            Juiz de Fora - MG
-            @break
-        @case ('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não indentificado
-    @endswitch
-    <hr>
+<hr>
+<h3>for</h3>
+@for($i = 0; isset($fornecedores[$i]); $i++)
+Fornecedor: {{ $fornecedores[$i]['nome'] }}
+<br>
+Status: {{ $fornecedores[$i]['status'] }}
+<br>
+CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+<br>
+Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+@switch($fornecedores[$i]['ddd'])
+@case ('11')
+São Paulo - SP
+@break
+@case ('32')
+Juiz de Fora - MG
+@break
+@case ('85')
+Fortaleza - CE
+@break
+@default
+Estado não indentificado
+@endswitch
+<hr>
 
-    @endfor
+@endfor
 @endisset
 <h3>while</h3>
 @isset($fornecedores)
 @php $i = 0 @endphp
-    @while(isset($fornecedores[$i]))
-        Fornecedor: {{ $fornecedores[$i]['nome'] }}
-        <br>
-        Status: {{ $fornecedores[$i]['status'] }}
-        <br>
-        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
-        <br>
-        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
-        <hr>
-        @php $i++ @endphp
-    @endwhile
-    @endisset
-    <h3>foreach</h3>
-    @isset($fornecedores)
-    @foreach ($fornecedores as $fornecedor)
-        
-            Fornecedor: {{ $fornecedor['nome'] }}
-            <br>
-            Status: {{ $fornecedor['status'] }}
-            <br>
-            CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
-            <br>
-            Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
-            <hr>
-            @endforeach
+@while(isset($fornecedores[$i]))
+Fornecedor: {{ $fornecedores[$i]['nome'] }}
+<br>
+Status: {{ $fornecedores[$i]['status'] }}
+<br>
+CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+<br>
+Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }}) {{ $fornecedores[$i]['telefone'] ?? '' }}
+<hr>
+@php $i++ @endphp
+@endwhile
+@endisset
+<h3>foreach</h3>
+@isset($fornecedores)
+@foreach ($fornecedores as $fornecedor)
 
-            @endisset
+Fornecedor: {{ $fornecedor['nome'] }}
+<br>
+Status: {{ $fornecedor['status'] }}
+<br>
+CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+<br>
+Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+<hr>
+@endforeach
+
+@endisset
+
+<h3>forelse</h3>
+@isset($fornecedores)
+@forelse ($fornecedores as $fornecedor)
+
+    Fornecedor: {{ $fornecedor['nome'] }}
+    <br>
+    Status: {{ $fornecedor['status'] }}
+    <br>
+    CNPJ: {{ $fornecedor['cnpj'] ?? '' }}
+    <br>
+    Telefone: ({{ $fornecedor['ddd'] ?? '' }}) {{ $fornecedor['telefone'] ?? '' }}
+    <hr>
+    @empty
+    "Array vazio"
+    @endforelse
+
+    @endisset
